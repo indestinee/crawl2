5362bf8cac2ff276baecdbbaac011254dd43478f
indestinee Chen <indestinee@gmail.com>
2018-05-26 17:25:37 +0800

    update

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ class Config:
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8', 
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+        'Connection': 'keep-alive'
     }
 cfg = Config()
